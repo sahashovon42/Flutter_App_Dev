d4245f3d557305b977a6e64095baabb3d16c0e6b
Shovon <75942431+sahashovon42@users.noreply.github.com>
2021-09-22 11:24:01 +0600

    list_2

---
+++ class_10/list_part2.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+  Map user1= {
+            "name":"Rahim",
+            "mail":"Karim@gmail.com"
+            };
+  List userList= [
+            {
+            "name":"Rahim",
+            "mail":"rahim@gmail.com"
+             },
+            {
+            "name":"Karim",
+            "mail":"karim@gmail.com"
+            },
+            {
+            "name":"Shovon",
+            "mail":"shovon@gmail.com"
+            },
+                 
+            ];
+  
+  print(userList[2]["name"]);
+  
+}
