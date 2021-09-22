aa3f5f884a6eb8cb7cf4c5faa692e7770f49a103
Shovon <75942431+sahashovon42@users.noreply.github.com>
2021-09-22 11:12:12 +0600

    List

---
+++ class_10/list_part1.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  List names= ['Rahim','Karim','Shovon','Jagat','Gourob'];
+  List ages= [22,21,23,25,20];
+  
+  for(int i=0; i<names.length; i++){
+  String name = names[i];
+  int age = ages[i];
+  print("$name is $age years old.");
+  }
+}
