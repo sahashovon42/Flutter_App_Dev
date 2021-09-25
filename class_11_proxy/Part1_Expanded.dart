5671826e1df7986e516b186f95f2672644fda32b
Shovon <75942431+sahashovon42@users.noreply.github.com>
2021-09-25 11:27:14 +0600

    Expanded_1

---
+++ class_11_proxy/Part1_Expanded.dart
@@ -0,0 +1,62 @@
+import 'package:class_11_proxy/home%20page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+
+
+//second page
+
+
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(flex: 5, child: Container(color: Colors.white70)),
+            Expanded(flex: 2, child: Container(color: Colors.red)),
+            Expanded(
+                flex: 2,
+                child: Container(
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 10, child: Container(color: Colors.blue)),
+                        Expanded(
+                            flex: 20, child: Container(color: Colors.black)),
+                        Expanded(
+                            flex: 40, child: Container(color: Colors.white70)),
+                        Expanded(
+                            flex: 20, child: Container(color: Colors.black)),
+                        Expanded(
+                            flex: 10, child: Container(color: Colors.blue)),
+                      ],
+                    ),
+                    color: Colors.purpleAccent)),
+            Expanded(flex: 2, child: Container(color: Colors.green)),
+            Expanded(flex: 2, child: Container(color: Colors.yellow)),
+          ],
+        ),
+      ),
+    );
+  }
+}
